bbfe7339f77257ebe1b01d0357c9b9e19255ee09
shane1595042264 <a1595042264@gmail.com>
2021-11-09 11:10:14 -0500

    Create OnlyStudy.js

---
+++ OnlyStudy.js
@@ -0,0 +1,77 @@
+function r(max) {
+    return Math.floor(Math.random() * max);
+}
+
+var Courses = [    
+"Psychology",
+"Environmental Science",
+"English Literature",
+"Spanish",
+"Math",
+"CS",
+]
+
+var Books = [
+"The Bean Trees",
+"Fahrenheit 451",
+"Wuthering Heights",
+"Poisonwood Bible",
+"Killing a Mocking Bird",
+"Hamlet"
+]
+
+var Computer_Books = [
+    "Eloquent JavaScript",
+    "Road to React",
+    "Python Algorithms",
+    "HTML CSS QuickStart Guide",
+    "Advanced Algorithms and Data Structures in Action"
+]
+
+var Exercises = [
+    "Breakdance",
+    "Locking",
+    "Hip-Hop",
+    "Popping",
+    "Basketball"
+]
+
+
+let bind = r(2);
+
+if(bind == 0){
+    console.log('Binding');
+}
+
+else{
+
+    console.log('Non-Binding');
+ 
+}
+
+
+    let a = r(2);
+    if(a==0){
+        let b = r(2);
+        if(b == 0){
+            console.log(`${Courses[r(Courses.length)]}`);
+
+        }
+        else{
+            let c = r(2);
+            if(c == 0){
+                console.log('AMC12');
+            }
+            else{console.log('College Application');}
+        }
+    }
+    else{
+        let d = r(2);
+        if(d == 0){
+            console.log(`Read ${Books[r(Books.length)]}`);
+        }
+        else{
+            console.log(`Read ${Computer_Books[r(Computer_Books.length)]}`);
+        }
+
+    }
